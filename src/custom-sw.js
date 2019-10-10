3368da931d49b8198352062670a1d333164ad0e2
Vignesh S <vs8@contractor.jcp.com>
2019-10-10 22:06:57 +0530

    added workbox

---
+++ src/custom-sw.js
@@ -0,0 +1,11 @@
+// See https://developers.google.com/web/tools/workbox/guides/configure-workbox
+workbox.setConfig({ debug: true });
+
+self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+
+// We need this in Webpack plugin (refer to swSrc option): https://developers.google.com/web/tools/workbox/modules/workbox-webpack-plugin#full_injectmanifest_config
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+// app-shell
+workbox.routing.registerRoute('/', new workbox.strategies.NetworkFirst());
